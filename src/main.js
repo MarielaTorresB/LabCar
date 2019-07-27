8e0f4cbccd588d3dd25233a9a9485c7b63e753a5
MarielaTorresB <martz_maru@hotmail.com>
2019-07-27 00:09:53 -0500

    Añadiendo la segunda sección

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ const dropdownMenu = document.getElementById("dropdownMenu");
 
 //Función showMenu = Al hacer click en el ícono de hamburguesa se muestra y oculta el menú desplegable
 const showMenu = () =>{
-   dropdownMenu.classList.toggle("hidden");
+dropdownMenu.classList.toggle("hidden");
 }
-
 
 
 logo.addEventListener("click", showMenu);
